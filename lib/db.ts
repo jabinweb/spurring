82accdbfbb21b50d08195adc1685ba3d2cbaaf5b
Harshit Singh <jabincreators@gmail.com>
2025-05-20 17:31:11 +0530

    Updated Home

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
 
