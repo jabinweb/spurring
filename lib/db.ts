4cfebca0a16c4b6e5919a4c5309fd0f9c2f320f1
Harshit Singh <jabincreators@gmail.com>
2025-05-20 17:17:45 +0530

    Updated Home

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient()
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
+
+export default prisma
 
 export async function getFirstAdmin() {
   return await prisma.user.findFirst({
