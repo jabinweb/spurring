e497ad0b2351f0e201bda323c818fd91087d62b0
Harshit Singh <jabincreators@gmail.com>
2025-08-21 22:14:15 +0530

    Added WhatsApp Button

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
 import { HiringPopup } from "@/components/hiring-popup";
+import { WhatsAppChat } from "@/components/whatsapp-button";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <main className="min-h-screen">{children}</main>
       <Footer />
       <HiringPopup />
+      <WhatsAppChat />
     </>
   );
 }
